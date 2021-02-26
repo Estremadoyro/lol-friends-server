34b52f9bda07ff55292cc8b73d1402782819c881
Estremadoyro <20160527@aloe.ulima.edu.pe>
2021-02-26 17:52:22 -0500

    region variables, yet to populate db on all servers

---
+++ misc/Variables.js
@@ -0,0 +1,57 @@
+export const selectRegion = {
+  regions: [
+    {
+      name: "LAS",
+      value: "la2",
+    },
+    {
+      name: "LAN",
+      value: "la1",
+    },
+    {
+      name: "NA",
+      value: "na1",
+    },
+    {
+      name: "BRASIL",
+      value: "br1",
+    },
+    {
+      name: "EUW",
+      value: "euw1",
+    },
+    {
+      name: "EUNE",
+      value: "eun1",
+    },
+    {
+      name: "RUSSIA",
+      value: "ru",
+    },
+    {
+      name: "TURKEY",
+      value: "tr1",
+    },
+    {
+      name: "JAPAN",
+      value: "jp1",
+    },
+  ],
+};
+
+export const selectRank = {
+  ranks: [
+    {
+      name: "CHALLENGER",
+      value: "CHALLENGER",
+    },
+    {
+      name: "GRANDMASTER",
+      value: "GRANDMASTER",
+    },
+    {
+      name: "MASTER",
+      value: "MASTER",
+    },
+  ],
+};
